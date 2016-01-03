67993e45523ba8574b7fee37222e4d46e35f0711
Stef van Buuren <stef.vanbuuren@tno.nl>
2016-01-03 09:49:20 +0100

    Add adp() function

--- data-raw/R/save_reference.R
+++ data-raw/R/save_reference.R
@@ -5,7 +5,7 @@ project <- path.expand("~/Package/dscore/dscore")
 datafile <- file.path(project, "data-raw/data/Dreference.txt")  # Created 13 Jun 2013
 
 # ------------- read data
-Dreference <- read.delim2(file = datafile)
+Dreference <- read.delim(file = datafile)
 
 # save to /data
 devtools::use_data(Dreference, overwrite = TRUE)

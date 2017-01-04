80508c9e7eae238e05b0a714777bc59e0142c8bb
Stef van Buuren <stef.vanbuuren@tno.nl>
2017-01-04 13:28:27 +0100

    test rm

--- scripts/rasch_on_all.R
+++ scripts/rasch_on_all.R
@@ -6,24 +6,24 @@ library("dplyr", warn.conflicts = FALSE)
 library("tidyr")
 
 # Obtain list of all items in gcdg
-all_items <- item_names()
 
-# select all items that are in range 0-1
-out_of_range <- check_items(ddata::gcdg, all_items)
-inrange_items <- dplyr::setdiff(all_items, out_of_range$item)
-
-dim(gcdg)
 
 # length(table(x)) == 2
 # min(table(x)) >= 2
 
-instruments <- unique(itemtable$instrument)
-for (i in 1:length(instruments)) {
-  ins <- instruments[i]
-  items <- filter(itemtable, instrument == ins) %>% .$item
-  items <- intersect(items, names(master))
-  scores <- select_(master, .dots = c("agedays", items))
-  nscores <- colSums(!is.na(scores))
-  # summarise only items with n > 100
-  scores <- select(scores, which(nscores > 100))
-}
+# Dutch items
+items <- item_names("NL")
+data <- select_(gcdg, .dots = all_items)
+fit <- rasch(data)
+get_diff(fit)
+# 
+# instruments <- unique(itemtable$instrument)
+# for (i in 1:length(instruments)) {
+#   ins <- instruments[i]
+#   items <- filter(itemtable, instrument == ins) %>% .$item
+#   items <- intersect(items, names(master))
+#   scores <- select_(master, .dots = c("agedays", items))
+#   nscores <- colSums(!is.na(scores))
+#   # summarise only items with n > 100
+#   scores <- select(scores, which(nscores > 100))
+# }

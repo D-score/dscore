c8b149a14381eb08e789ff2ffed274008f901561
Stef van Buuren <stef.vanbuuren@tno.nl>
2017-11-27 08:51:17 +0100

    Adapt parameters in count_mu_gcdg()

--- R/dscore.R
+++ R/dscore.R
@@ -388,7 +388,7 @@ adp <- function(age, qp = -10:100, mu = "dutch", sd = 5,
 }
 
 count_mu_dutch <- function(t) {44.35 - 1.8 * t + 28.47 * log(t + 0.25)}
-count_mu_gcdg  <- function(t) {46.90 - 2.36 * t + 26.11 * log(t + 0.19)}
+count_mu_gcdg  <- function(t) {47.65 - 3.05 * t + 26.70 * log(t + 0.19)}
 
 #' Normalize distribution
 #'

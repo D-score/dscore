5317c91f0e30a4274504b3e7c962d3a6b6528ca8
Stef van Buuren <stef.vanbuuren@tno.nl>
2018-06-22 23:07:17 +0200

    Add test to compare estimates from dscore() and ability()

--- tests/testthat/test-ability.R
+++ tests/testthat/test-ability.R
@@ -1,4 +1,4 @@
-context("ability:check")
+context("ability")
 
 items <- names(popsdemo)[8:64]
 itemset <- !is.na(itembank$lex_dutch1983)
@@ -11,8 +11,32 @@ delta <- gettau(items = names(popsdemo)[item_locations])
 items <- names(delta)
 
 data <- popsdemo
-data$age <- round(data$age/365.25, 3)
-key <- data.frame(item = items, 
-                  delta = delta)
+data$age <- round(data$daycor / 365.25, 3)
 
-z <- ability(data, items, age = "age", key = key)
+key <- data.frame(item = items, 
+                  delta = delta, 
+                  stringsAsFactors = FALSE)
+
+abil <- ability(data, items, age = "age", key = key)
+
+# --- using dscore()
+
+dsco <- data %>%
+  select(patid, age, one_of(items)) %>%
+  gather(item, score, -age, -patid, na.rm = TRUE) %>%
+  arrange(patid, age) %>%
+  group_by(patid, age) %>%
+  summarise(d = dscore::dscore(scores = score, items = item,
+                               ages = age, 
+                               mu = "gcdg",
+                               itembank = dscore::itembank, 
+                               lexicon = "ghap")) %>%
+  ungroup()
+
+test_that("ability() and dscore() produce same number of rows", {
+  expect_identical(nrow(abil), nrow(dsco))
+})
+
+test_that("ability() and dscore() produce same D-scores", {
+  expect_identical(abil$ability, dsco$d)
+})

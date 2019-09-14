117351f21a6bfbac525926729ee21aba603ef965
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-09-14 16:16:28 +0200

    Define basic information

--- data-raw/R/save_builtin_itembank.R
+++ data-raw/R/save_builtin_itembank.R
@@ -1,11 +1,28 @@
-# Saves the itembankVWO4 datasets to /data
+# Creates the built-in itembank
+library(openxlsx)
 
 # define project
 project <- path.expand("~/Package/dscore/dscore")
-datafile <- file.path(project, "data-raw/data/itembankVWO1.txt")  # May 27, 2019
 
-# ------------- read data
-builtin_itembank <- read.delim(file = datafile, stringsAsFactors = FALSE)
+# ------------- dutch model
+fn <- file.path(project, "data-raw/data/itembankVWO1.txt")  # May 27, 2019
+ib_dutch <- read.delim(file = fn, stringsAsFactors = FALSE)
+ib_dutch$tau_dutch <- ib_dutch$tau
+ib_dutch$tau <- NULL
+
+# ------------- gcdg model
+fn  <- path.expand("~/Package/dscore/dscore/data-raw/data/gcdg_itembank.txt")
+ib_gcdg <- read.delim(fn)
+ib_gcdg$tau_gcdg <- ib_gcdg$tau
+ib_gcdg$tau <- NULL
+
+# ------------- gsed model
+fn <- path.expand("~/Project/GSED/dmetric/models/807_17/model.Rds")
+gsed_model_807_17 <- readRDS(file = fn)
+ib_gsed <- gsed_model_807_17$itembank
+ib_gsed$tau_gsed <- ib_gsed$tau
+ib_gsed$tau <- NULL
+
 
 # save to /data
-usethis::use_data(builtin_itembank, overwrite = TRUE)
+# usethis::use_data(builtin_itembank, overwrite = TRUE)

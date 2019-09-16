8032bee034187042a1e2238d3e3fe475731e7830
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-09-16 22:56:39 +0200

    Collect internal functions

--- R/count.R
+++
@@ -1,2 +0,0 @@
-count_mu_dutch <- function(t) {44.35 - 1.8 * t + 28.47 * log(t + 0.25)}
-count_mu_gcdg  <- function(t) {47.65 - 3.05 * t + 26.70 * log(t + 0.19)}
--- R/internal.R
+++ R/internal.R
@@ -1,3 +1,6 @@
+count_mu_dutch <- function(t) {44.35 - 1.8 * t + 28.47 * log(t + 0.25)}
+count_mu_gcdg  <- function(t) {47.65 - 3.05 * t + 26.70 * log(t + 0.19)}
+
 set_reference <- function(key = "gsed") {
   if (key == "gsed") key <- "gcdg"
   dscore::builtin_references[dscore::builtin_references$pop == key, ]

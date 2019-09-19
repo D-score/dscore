24a4bb9e993ff149eb36b81c269d0e2437330ae3
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-09-19 23:35:09 +0200

    Replace reference by population parameter

--- R/internal.R
+++ R/internal.R
@@ -1,7 +1,2 @@
 count_mu_dutch <- function(t) {44.35 - 1.8 * t + 28.47 * log(t + 0.25)}
 count_mu_gcdg  <- function(t) {47.65 - 3.05 * t + 26.70 * log(t + 0.19)}
-
-set_reference <- function(pop = "gcdg") {
-  if (pop == "gsed") pop <- "gcdg"
-  dscore::builtin_references[dscore::builtin_references$pop == pop, ]
-}

a7e28b911db687ad0e29fb6673c381d499768e43
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-09-23 21:00:20 +0200

    Copy rename_gcdg_gsed.R from gseddata

--- R/import.R
+++ R/import.R
@@ -2,5 +2,6 @@
 #'                       mutate n recode select summarise ungroup %>%
 #' @importFrom tidyr     gather unnest
 #' @importFrom stats     approx dnorm plogis weighted.mean
+#' @importFrom stringr   str_pad
 #' @importFrom rlang     .data
 NULL

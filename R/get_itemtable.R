95a1f61f375efa1cc9d886483214c7627faeadc3
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-02-26 11:44:11 +0100

    Add creation of dynamic item table

--- R/get_itemtable.R
+++ R/get_itemtable.R
@@ -5,18 +5,28 @@
 #' the instrument, the domain and the item label.
 #' The `get_itemtable()` function returns a subset of `items`
 #' in the itemtable.
-#' @inheritParams get_itemnames
 #' @return A `data.frame` with seven columns.
 #' @param items A logical or character vector of item names to return. The
 #' default (`NULL`) returns all items.
+#' @param itemtable A `data.frame` set up according to the
+#' same structure as [builtin_itemtable()]. If not specified,
+#' the `builtin_itemtable` is used. If `itemtable = ""`, then
+#' a dynamic item table is created from any specified item names.
 #' @seealso [get_labels()], [get_itemnames()]
+#' @examples
+#' head(get_itemtable(), 3)
+#' get_itemtable(LETTERS[1:3], "")
 #' @export
 get_itemtable <- function(items = NULL, itemtable = NULL) {
 
-  # construct variable names
   if (is.null(itemtable)) itemtable <- dscore::builtin_itemtable
-  if (is.null(items)) {
-    return(itemtable)
-  }
+  else if (itemtable == "" && length(items))
+    return(data.frame(item = items,
+                      instrument = NA_character_,
+                      equate = NA_character_,
+                      domain = NA_character_,
+                      label = paste("Label for", items),
+                      stringsAsFactors = FALSE))
+  if (is.null(items)) return(itemtable)
   itemtable[itemtable$item %in% items, ]
 }

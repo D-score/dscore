5c3e2da62ee44b017c4626a029295dfa5f1d153e
Stef van Buuren <stef.vanbuuren@tno.nl>
2022-08-12 09:37:38 +0200

    Adapt BCT functions to work with missing and out-of-range data

--- R/internal.R
+++ R/internal.R
@@ -1,4 +1,4 @@
-# Two functions from gamlss.dist
+# Two functions adapted from gamlss.dist to work better with out-of-range data
 #
 # To cite package ‘gamlss.dist’ in publications use:
 # Stasinopoulos M, Rigby R (2022). _gamlss.dist: Distributions for
@@ -14,18 +14,18 @@
 #   url = {https://CRAN.R-project.org/package=gamlss.dist}}
 
 qBCT <- function (p, mu = 5, sigma = 0.1, nu = 1, tau = 2, lower.tail = TRUE,
-                  log.p = FALSE)
+                  log.p = FALSE, na.rm = TRUE)
 {
-  if (any(mu < 0))
+  if (any(mu < 0, na.rm = na.rm))
     stop(paste("mu must be positive", "\n", ""))
-  if (any(sigma < 0))
+  if (any(sigma < 0, na.rm = na.rm))
     stop(paste("sigma must be positive", "\n", ""))
-  if (any(tau < 0))
+  if (any(tau < 0, na.rm = na.rm))
     stop(paste("tau must be positive", "\n", ""))
   if (log.p == TRUE)
     p <- exp(p)
   else p <- p
-  if (any(p <= 0) | any(p >= 1))
+  if (any(p <= 0, na.rm = na.rm) | any(p >= 1, na.rm = na.rm))
     stop(paste("p must be between 0 and 1", "\n", ""))
   if (lower.tail == TRUE)
     p <- p
@@ -46,21 +46,23 @@ qBCT <- function (p, mu = 5, sigma = 0.1, nu = 1, tau = 2, lower.tail = TRUE,
   else if (nu != 0)
     ya <- mu * (nu * sigma * z + 1)^(1/nu)
   else ya <- mu * exp(sigma * z)
+  # Round up D-scores < 1 to 1.0
+  ya <- ya[ya < 1.0 & !is.na(ya)] <- 1.0
   ya
 }
 
 
 pBCT <- function (q, mu = 5, sigma = 0.1, nu = 1, tau = 2, lower.tail = TRUE,
-                  log.p = FALSE)
+                  log.p = FALSE, na.rm = TRUE)
 {
-  if (any(mu < 0))
+  if (any(mu < 0, na.rm = na.rm))
     stop(paste("mu must be positive", "\n", ""))
-  if (any(sigma < 0))
+  if (any(sigma < 0, na.rm = na.rm))
     stop(paste("sigma must be positive", "\n", ""))
-  if (any(tau < 0))
+  if (any(tau < 0, na.rm = na.rm))
     stop(paste("tau must be positive", "\n", ""))
-  if (any(q < 0))
-    stop(paste("y must be positive", "\n", ""))
+  # recode D-score < 1 to 1.
+  q[q < 1.0 & !is.na(q)] <- 1.0
   if (length(nu) > 1)
     z <- ifelse(nu != 0, (((q/mu)^nu - 1)/(nu * sigma)),
                 log(q/mu)/sigma)

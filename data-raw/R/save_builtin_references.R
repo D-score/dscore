bd843d2b13a481a12991639609e42d9a43492fd3
Iris Eekhout <iriseekhout@gmail.com>
2024-04-11 17:15:51 +0200

    add dutch reference for the gsed2212 key.
    filtered by pop == "dutch_gsed2212"

--- data-raw/R/save_builtin_references.R
+++ data-raw/R/save_builtin_references.R
@@ -6,6 +6,7 @@ path <- file.path("data-raw/data/references")
 f1 <- file.path(path, "dutch.txt")
 f2 <- file.path(path, "gcdg.txt")
 f3 <- file.path(path, "phase1.txt")
+f4 <- file.path(path, "Dutch_gsed2212.txt")
 
 # ------------- dutch references
 ref_dutch <- read.delim(file = f1) %>%
@@ -46,8 +47,25 @@ ref_phase1 <- read.delim(file = f3) %>%
          SDP2 = dscore:::qBCT(pnorm(+2), mu, sigma, nu, tau)
   )
 
+# ------------- phase1 references
+ref_dutchgsed <- read.delim(file = f4) %>%
+  mutate(pop = "dutch_gsed2212",
+         P3 = dscore:::qBCT(0.03, mu, sigma, nu, tau),
+         P10 = dscore:::qBCT(0.10, mu, sigma, nu, tau),
+         P25 = dscore:::qBCT(0.25, mu, sigma, nu, tau),
+         P50 = dscore:::qBCT(0.50, mu, sigma, nu, tau),
+         P75 = dscore:::qBCT(0.75, mu, sigma, nu, tau),
+         P90 = dscore:::qBCT(0.90, mu, sigma, nu, tau),
+         P97 = dscore:::qBCT(0.97, mu, sigma, nu, tau),
+         SDM2 = dscore:::qBCT(pnorm(-2), mu, sigma, nu, tau),
+         SDM1 = dscore:::qBCT(pnorm(-1), mu, sigma, nu, tau),
+         SD0 = dscore:::qBCT(pnorm(-0), mu, sigma, nu, tau),
+         SDP1 = dscore:::qBCT(pnorm(+1), mu, sigma, nu, tau),
+         SDP2 = dscore:::qBCT(pnorm(+2), mu, sigma, nu, tau)
+  )
+
 # save to /data
-builtin_references <- bind_rows(ref_dutch, ref_gcdg, ref_phase1) %>%
+builtin_references <- bind_rows(ref_dutch, ref_gcdg, ref_phase1, ref_dutchgsed) %>%
   rename(age = year) %>%
   dplyr::select(
     pop, age, mu, sigma, nu, tau,

9d60f614eecad0c42d5b6367dc878431a5668e70
Stef van Buuren <stef.vanbuuren@tno.nl>
2025-10-01 22:04:56 +0200

    Add gsed2510 key, deprecate 293_0 key

--- data-raw/R/save_builtin_keys.R
+++ data-raw/R/save_builtin_keys.R
@@ -11,7 +11,8 @@ builtin_keys <- data.frame(
     "293_0",
     "gsed2208",
     "gsed2212",
-    "gsed2406"
+    "gsed2406",
+    "gsed2510"
   ),
   base_population = c(
     "dutch",
@@ -24,10 +25,11 @@ builtin_keys <- data.frame(
     "phase1",
     "phase1",
     "phase1",
+    "preliminary_standards",
     "preliminary_standards"
   ),
-  n_items = c(76, 565, 945, 278, 310, 1126, 606, 293, 1407, 1478, 1478),
-  n_instruments = c(1, 13, 21, 7, 13, 22, 20, 2, 25, 27, 27),
+  n_items = c(76, 565, 945, 278, 310, 1126, 606, 293, 1407, 1478, 1478, 281),
+  n_instruments = c(1, 13, 20, 7, 13, 22, 20, 2, 25, 23, 23, 2),
   intercept = c(
     38.906,
     66.483757,
@@ -39,7 +41,8 @@ builtin_keys <- data.frame(
     54.939147,
     54.939147,
     54.939147,
-    54.939147
+    54.939147,
+    55.724132
   ),
   slope = c(
     2.1044,
@@ -52,11 +55,12 @@ builtin_keys <- data.frame(
     4.064264,
     4.064264,
     4.064264,
-    4.064264
+    4.064264,
+    3.603965
   ),
-  from = rep(-10, 11),
-  to = c(80, rep(100, 10)),
-  by = rep(1, 11),
+  from = rep(-10, 12),
+  to = c(80, rep(100, 11)),
+  by = rep(1, 12),
   retired = c(
     FALSE,
     FALSE,
@@ -65,16 +69,31 @@ builtin_keys <- data.frame(
     TRUE,
     TRUE,
     TRUE,
+    TRUE,
+    TRUE,
+    TRUE,
     FALSE,
-    TRUE,
-    TRUE,
     FALSE
+  ),
+  reference = c(
+    "van Buuren 2014",
+    "Weber 2019",
+    "Van Buuren 2025",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "Van Buuren 2025",
+    "Van Buuren 2025",
+    "In preparation"
   ),
   stringsAsFactors = FALSE
 )
 
 # remove retired keys
-rem <- c("sf2206", "lf2206", "294_0", "gsed2206", "gsed2208")
+rem <- c("sf2206", "lf2206", "294_0", "293_0", "gsed2206", "gsed2208")
 builtin_keys <- builtin_keys[!builtin_keys$key %in% rem, ]
 
 usethis::use_data(builtin_keys, overwrite = TRUE)
